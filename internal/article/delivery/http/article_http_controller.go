7501d5d6382db502b643f208387a1d1b3822b92c
Diki Haryadi <dk.harryadi@gmail.com>
2024-12-01 06:20:07 +0700

    refactor: update response structure
    This commit contain
    1. update response structure
    2. add db seeder
    3. add cobra command

--- internal/article/delivery/http/article_http_controller.go
+++ internal/article/delivery/http/article_http_controller.go
@@ -1,13 +1,11 @@
 package articleHttpController
 
 import (
-	"net/http"
-
+	"github.com/diki-haryadi/go-micro-template/pkg/response"
 	"github.com/labstack/echo/v4"
 
 	articleDomain "github.com/diki-haryadi/go-micro-template/internal/article/domain"
 	articleDto "github.com/diki-haryadi/go-micro-template/internal/article/dto"
-	articleException "github.com/diki-haryadi/go-micro-template/internal/article/exception"
 )
 
 type controller struct {
@@ -21,19 +19,24 @@ func NewController(uc articleDomain.UseCase) articleDomain.HttpController {
 }
 
 func (c controller) CreateArticle(ctx echo.Context) error {
+	res := response.NewJSONResponse()
 	aDto := new(articleDto.CreateArticleRequestDto)
 	if err := ctx.Bind(aDto); err != nil {
-		return articleException.ArticleBindingExc()
+		res.SetError(response.ErrBadRequest).SetMessage(err.Error()).Send(ctx.Response().Writer)
+		return nil
 	}
 
 	if err := aDto.ValidateCreateArticleDto(); err != nil {
-		return articleException.CreateArticleValidationExc(err)
+		res.SetError(response.ErrBadRequest).SetMessage(err.Error()).Send(ctx.Response().Writer)
+		return nil
 	}
 
 	article, err := c.useCase.CreateArticle(ctx.Request().Context(), aDto)
 	if err != nil {
-		return err
+		res.SetError(response.ErrBadRequest).SetMessage(err.Error()).Send(ctx.Response().Writer)
+		return nil
 	}
 
-	return ctx.JSON(http.StatusOK, article)
+	res.APIStatusSuccess().SetData(article).Send(ctx.Response().Writer)
+	return nil
 }
